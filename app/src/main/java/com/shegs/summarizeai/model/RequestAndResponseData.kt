c4c28d679b941d96b1695633ed6c3bbcec22ab44
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-10-06 11:40:35 +0100

    Request and response data class added

--- app/src/main/java/com/shegs/summarizeai/model/RequestAndResponseData.kt
+++ app/src/main/java/com/shegs/summarizeai/model/RequestAndResponseData.kt
@@ -1,2 +1,15 @@
 package com.shegs.summarizeai.model
 
+data class OpenAIRequest(
+    val model: String,
+    val prompt: String,
+    val max_tokens: Int,
+    val temperature: Int,
+    val top_p: Int,
+    val frequency_penalty: Int,
+    val presence_penalty: Int
+)
+
+data class OpenAIResponse(val choices: List<Choice>)
+
+data class Choice(val text: String)
