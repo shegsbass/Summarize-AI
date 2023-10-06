431d568add420d6a82dda6f4567bf42af569df8d
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-10-06 11:44:17 +0100

    Function to summarize texts added

--- app/src/main/java/com/shegs/summarizeai/SummarizationFunction.kt
+++ app/src/main/java/com/shegs/summarizeai/SummarizationFunction.kt
@@ -6,6 +6,38 @@ import com.shegs.summarizeai.network.RetrofitClient.openAIApiService
 import retrofit2.Call
 
 class SummarizationFunction {
+    fun summarizeText(apiKey: String, inputText: String) {
+        val request = OpenAIRequest(
+            "text-davinci-003",
+            "Summarize the following text in 2-4 sentences: $inputText",
+            256,
+            1,
+            1,
+            0,
+            0
+        )
+        val call = openAIApiService.summarizeText("Bearer $apiKey", request)
 
+        call.enqueue(
+            object : retrofit2.Callback<OpenAIResponse> {
+                override fun onResponse(
+                    call: Call<OpenAIResponse>,
+                    response: retrofit2.Response<OpenAIResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val summarizedText = response.body()?.choices?.get(0)?.text
+                        println("Summarized Text:")
+                        println(summarizedText)
+                    } else {
+                        println("Request failed with status code: ${response.code()}")
+                        println("Response body: ${response.errorBody()?.string()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<OpenAIResponse>, t: Throwable) {
+                    println("Request failed: ${t.message}")
+                }
+            })
+    }
 
 }
